fa67f74a23bb9b8f9d9dc88ca7d837f1e8ae5584
gyeongmin <gyeong-min.park@kt.com>
2024-01-29 17:55:11 +0900

    도카설정 및 action

--- app/src/main/java/com/study/dokka/MainActivity.kt
+++ app/src/main/java/com/study/dokka/MainActivity.kt
@@ -3,6 +3,9 @@ package com.study.dokka
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * 메인 액티비티 테스트 입니다.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
